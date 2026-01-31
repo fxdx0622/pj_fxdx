66e33775af234f02425396c8f7b6268c83c11625
fxdx1999 <fxdx0622@gmail.com>
2026-02-01 01:16:36 +0900

    ADD: RenderGraph(DOF 2D) 移植

---
+++ pj_fxdx/Assets/Workspace/RenderGraph/DrawTransparentDepthRenderPass.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine.Rendering.RendererUtils;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+
+public class DrawTransparentDepthRenderPass : ScriptableRenderPass
+{
+	private readonly ShaderTagId shaderTagId = new ShaderTagId("TransparentDepthOnly");
+
+	private class PassData
+	{
+		public RendererListHandle list;
+	}
+
+	public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frame)
+	{
+		UniversalCameraData cameraData = frame.Get<UniversalCameraData>();
+		UniversalResourceData resourceData = frame.Get<UniversalResourceData>();
+		UniversalRenderingData renderingData = frame.Get<UniversalRenderingData>();
+
+		RendererListDesc rlDesc = new RendererListDesc(shaderTagId, renderingData.cullResults, cameraData.camera);
+		rlDesc.renderQueueRange = RenderQueueRange.transparent;
+		rlDesc.sortingCriteria = SortingCriteria.CommonTransparent;
+		rlDesc.layerMask = cameraData.camera.cullingMask;
+		using IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass<PassData>("Draw Transparent Depth", out PassData passData);
+
+		passData.list = renderGraph.CreateRendererList(rlDesc);
+		builder.UseRendererList(passData.list);
+
+		builder.SetRenderAttachmentDepth(resourceData.activeDepthTexture, AccessFlags.Write);
+		builder.SetRenderFunc(static (PassData d, RasterGraphContext ctx) => { ctx.cmd.DrawRendererList(d.list); });
+	}
+}
---
+++ pj_fxdx/Assets/Workspace/RenderGraph/DrawTransparentDepthRendererFeature.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DrawTransparentDepthRendererFeature : ScriptableRendererFeature
+{
+	[SerializeField] private RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+	private DrawTransparentDepthRenderPass pass;
+
+	public override void Create()
+	{
+		pass = new DrawTransparentDepthRenderPass()
+		{
+			renderPassEvent = renderPassEvent,
+		};
+	}
+
+	public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+	{
+		if (pass != null) renderer.EnqueuePass(pass);
+	}
+}
